cce17b8ad21bff55144cd5b1b8700e4db54e9a65
AMMAR839 <ammaryasir839@gmail.com>
2025-09-15 21:44:20 +0700

    Initial commit

---
+++ controllers/authController.js
@@ -0,0 +1,18 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req,res)=>{
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if(!user) return res.status(400).json({ message:'User tidak ditemukan' });
+    const match = await bcrypt.compare(password, user.password);
+    if(!match) return res.status(400).json({ message:'Password salah' });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    res.json({ token, username: user.username, role: user.role });
+  } catch(err){
+    res.status(500).json({ message: err.message });
+  }
+};
