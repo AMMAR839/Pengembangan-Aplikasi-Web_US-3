3c5a53631a88081130a89891205a04cf15b15e85
Alganawi <mirsadalganawi92@gmail.com>
2025-09-25 11:43:42 +0700

    Update messageController.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,55 +1,32 @@
 const Message = require('../models/Message');
-const Student = require('../models/Student');
-
 
 exports.sendMessage = async (req, res) => {
   try {
-    const { studentId, message, type } = req.body;
-    
-  
-    const student = await Student.findById(studentId);
-    if (!student) {
-      return res.status(404).json({ message: 'Siswa tidak ditemukan' });
+    const { parentId, studentId, content } = req.body;
+
+    if (req.user.role !== 'admin') {
+      return res.status(403).json({ message: 'Hanya guru/admin yang bisa mengirim pesan' });
     }
 
-    const newMessage = new Message({
-      studentId,
+    const msg = await Message.create({
       teacherId: req.user._id,
-      message,
-      type: type || 'umum'
+      parentId,
+      studentId,
+      content
     });
 
-    await newMessage.save();
-    res.status(201).json({ message: 'Pesan berhasil dikirim', data: newMessage });
+    res.status(201).json({ message: "Pesan berhasil dikirim", data: msg });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
-
-exports.getMessages = async (req, res) => {
+exports.getMessagesForParent = async (req, res) => {
   try {
-
-    const students = await Student.find({ parentUserId: req.user._id });
-    const studentIds = students.map(student => student._id);
-
-    const messages = await Message.find({ studentId: { $in: studentIds } })
+    const messages = await Message.find({ parentId: req.user._id })
       .populate('teacherId', 'username')
-      .populate('studentId', 'nama')
-      .sort({ date: -1 });
-
+      .populate('studentId', 'nama');
     res.json(messages);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-
-exports.markAsRead = async (req, res) => {
-  try {
-    const { messageId } = req.params;
-    await Message.findByIdAndUpdate(messageId, { isRead: true });
-    res.json({ message: 'Pesan ditandai sebagai sudah dibaca' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
