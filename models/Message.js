b0cad6eb49558185c3a0c5408268e00c2e900d13
Alganawi <mirsadalganawi92@gmail.com>
2025-09-25 11:40:36 +0700

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
   teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  message: { type: String, required: true },
-  type: { type: String, enum: ['perkembangan', 'kesehatan', 'umum'], default: 'umum' },
-  isRead: { type: Boolean, default: false },
-  date: { type: Date, default: Date.now }
+  parentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
